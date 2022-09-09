b622f3b10b2e0cf3a3559c126ab6c7eeb0029701
williameom <66811147+williameom5678@users.noreply.github.com>
2022-09-09 10:07:18 +0900

    editconfig 버전 샹향

--- kakaobot.js
+++ kakaobot.js
@@ -63,8 +63,8 @@ let defConfig = {
     // eslint-disable-next-line max-len
     locoPEMPublicKey: `-----BEGIN PUBLIC KEY-----\nMIIBIDANBgkqhkiG9w0BAQEFAAOCAQ0AMIIBCAKCAQEApElgRBx+g7sniYFW7LE8ivrwXShKTRFV8lXNItMXbN5QSC8vJ/cTSOTS619Xv5Zx7xXJIk4EKxtWesEGbgZpEUP2xQ+IeH9oz0JxayEMvvD1nVNAWgpWE4pociEoArsK7qY3YwXb1CiDHo9hojLv7djbo3cwXvlyMh4TUrX2RjCZPlVJxk/LVjzcl9ohJLkl3eoSrf0AE4kQ9mk3+raEhq5Dv+IDxKYX+fIytUWKmrQJusjtre9oVUX5sBOYZ0dzez/XapusEhUWImmB6mciVXfRXQ8IK4IH6vfNyxMSOTfLEhRYN2SMLzplAYFiMV536tLS3VmG5GJRdkpDubqPeQIBAw==\n-----END PUBLIC KEY-----`,
     agent: 'win32',
-    version: '3.3.8',
-    appVersion: '3.3.8.3058',
+    version: '3.4.2',
+    appVersion: '3.4.2.3187',
     osVersion: '10.0',
     // 2 == sub, 1 == main
     deviceType: 2,
@@ -84,8 +84,8 @@ let editConfig = {
     // eslint-disable-next-line max-len
     locoPEMPublicKey: `-----BEGIN PUBLIC KEY-----\nMIIBIDANBgkqhkiG9w0BAQEFAAOCAQ0AMIIBCAKCAQEApElgRBx+g7sniYFW7LE8ivrwXShKTRFV8lXNItMXbN5QSC8vJ/cTSOTS619Xv5Zx7xXJIk4EKxtWesEGbgZpEUP2xQ+IeH9oz0JxayEMvvD1nVNAWgpWE4pociEoArsK7qY3YwXb1CiDHo9hojLv7djbo3cwXvlyMh4TUrX2RjCZPlVJxk/LVjzcl9ohJLkl3eoSrf0AE4kQ9mk3+raEhq5Dv+IDxKYX+fIytUWKmrQJusjtre9oVUX5sBOYZ0dzez/XapusEhUWImmB6mciVXfRXQ8IK4IH6vfNyxMSOTfLEhRYN2SMLzplAYFiMV536tLS3VmG5GJRdkpDubqPeQIBAw==\n-----END PUBLIC KEY-----`,
     agent: 'win32',
-    version: '3.3.8',
-    appVersion: '3.3.8.3058',
+    version: '3.4.2',
+    appVersion: '3.4.2.3187',
     osVersion: '10.0',
     // 2 == sub, 1 == main
     deviceType: 2,
