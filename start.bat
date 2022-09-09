14c503ca1b831e4988a0a216972e837fe1c333bc
williameom <williameom5678@gmail.com>
2022-09-09 09:55:12 +0900

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001
-title Node Kakao Bot [ Telegram: @ow9kj1w ] [ Discord: williameom_#6512 ]
+title Node Kakao Bot [ Telegram: @williameom ] [ Discord: williameom_#6512 ]
 cls
 npm run start
 pause
